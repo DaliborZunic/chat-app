27cc7b7b9fe13d557f3c62c40aa6291d9f825d77
programer10 <programer10@ucione.local>
2022-03-28 19:57:31 +0200

    Edit bubble UI

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -6,9 +6,19 @@ const Message = (props) => {
 
   return (
 
-    <div className= {`Message ${props.sentByMe === true ? "sent-by-me" : null}`}>
-      <span className="message-sender">{props.user}</span>
-      <span className="message-body">{props.messageBody}</span>
+    <div className={`Message ${props.sentByMe === true ? "sent-by-me" : null}`}>
+
+      <div className="avatar-column">
+        <div className="avatar"></div>
+      </div>
+
+      <div className="message-column">
+
+        <span className="message-sender">{props.user}</span>
+        <span className="message-body">{props.messageBody}</span>
+      </div>
+
+
     </div>
   );
 };
