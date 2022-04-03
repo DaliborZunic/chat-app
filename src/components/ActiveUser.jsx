385b7f71936e44dc8a7f1398e147cac550aeea8b
Dalibor Žunić <dalibor.zunic2@gmail.com>
2022-04-04 00:54:28 +0200

    Animate is typing status

--- src/components/ActiveUser.jsx
+++ src/components/ActiveUser.jsx
@@ -1,32 +1,34 @@
 import detective from "../images/detective.svg";
 import astronaut from "../images/astronaut.svg";
 import ninja from "../images/ninja.svg";
+import isTyping from "../images/is-typing.svg";
 
 const ActiveUser = (props) => {
+  const setAvatar = () => {
+    if (props.avatar === "detective") {
+      return detective;
+    }
 
-    const setAvatar = () => {
-        if (props.avatar === "detective") {
-          return detective
-        }
-    
-        if (props.avatar === "astronaut") {
-          return astronaut
-        }
-    
-        if (props.avatar === "ninja") {
-          return ninja
-        }
-      }
+    if (props.avatar === "astronaut") {
+      return astronaut;
+    }
+
+    if (props.avatar === "ninja") {
+      return ninja;
+    }
+  };
 
   return (
     <div className={`ActiveUser ${props.currentUser ? "currentUser" : ""}`}>
+      <div className="sidebar-avatar-frame">
+        <img className="active-user-avatar-icon" src={setAvatar()} alt="" />
+      </div>
 
-        <div className="sidebar-avatar-frame">
-            <img className="active-user-avatar-icon" src={setAvatar()} alt="" />
-        </div>
-      
       <span className="sidebar-username">{props.username}</span>
-      <span>{props.currentlyTyping.includes(props.activeUser) ? "is typing" : ""}</span>
+
+      {props.currentlyTyping.includes(props.activeUser) && (
+        <img className="is-typing-animation" src={isTyping} alt="" />
+      )}
     </div>
   );
 };
